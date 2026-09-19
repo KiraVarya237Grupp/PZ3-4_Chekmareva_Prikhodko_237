220b677629d20eb80c615973ca848b3525c2be8a
chekmarevarya07-hub <chekmarevarya.07@gmail.com>
2026-09-19 12:10:41 +0300

    Delete django_auth directory

--- django_auth/accounts/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    # Путь 'profile/' удален или закомментирован
-    path('courses/', views.course_list, name='course_list'),
-]
